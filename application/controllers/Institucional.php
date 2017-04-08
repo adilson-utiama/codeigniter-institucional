3263b4cca86310b9c7485adceb74f021d434e613
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-04-08 17:24:33 -0300

    Commit innicia

---
+++ application/controllers/Institucional.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Institucional extends CI_Controller {
+
+    public function index(){
+
+        $this->load->view('home');
+    }
+
+    public function Empresa() {
+
+        $this->load->view('commons/header');
+        $this->load->view('empresa');
+        $this->load->view('commons/footer');
+    }
+
+    public function Servicos() {
+        $this->load->view('commons/header');
+        $this->load->view('servicos');
+        $this->load->view('commons/footer');
+    }
+
+
+}
+
+?>
---
+++ application/views/empresa.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="page-header">
+        <h1>A Empresa</h1>
+    </div>
+    <p class="lead">Voce esta criando a pagina com as informacoes da empresa</p>
+    <p>Essa e uma pagina simples , contendo cabecalho e rodape</p>
+
+</div>
