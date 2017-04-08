d1ed1d329ea41c3e7ca93e5f78b5d49d310e8438
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-04-08 18:23:48 -0300

    cacheando paginas

--- application/controllers/Institucional.php
+++ application/controllers/Institucional.php
@@ -3,30 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Institucional extends CI_Controller {
 
-    public function index(){
-        $data['title'] = "LCI - HOME";
-        $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
+  //Cacheando todas as funcoes que retornam uma "view"
+  public function __construct() {
+      parent::__construct();
+      $this->output->cache(1440);
+  }
 
-        $this->load->view('home', $data);
-    }
+  public function index(){
+      $data['title'] = "LCI - HOME";
+      $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
 
-    public function Empresa() {
-        $data['title'] = "LCI - HOME";
-        $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
+      $this->load->view('home', $data);
+  }
 
-        $this->load->view('commons/header', $data);
-        $this->load->view('empresa');
-        $this->load->view('commons/footer');
-    }
+  public function Empresa() {
+      $data['title'] = "LCI - HOME";
+      $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
 
-    public function Servicos() {
-        $data['title'] = "LCI - HOME";
-        $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
+      $this->load->view('commons/header', $data);
+      $this->load->view('empresa');
+      $this->load->view('commons/footer');
+  }
 
-        $this->load->view('commons/header', $data);
-        $this->load->view('servicos');
-        $this->load->view('commons/footer');
-    }
+  public function Servicos() {
+      $data['title'] = "LCI - HOME";
+      $data['description'] = "Exercicio de exemplo do capitulo 5 do livro Codeigniter";
+
+      $this->load->view('commons/header', $data);
+      $this->load->view('servicos');
+      $this->load->view('commons/footer');
+  }
 
 
 }
